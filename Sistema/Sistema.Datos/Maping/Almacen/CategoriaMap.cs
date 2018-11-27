68d821a8d3d6b8b1aa3c27eaab6f05dc334ff26e
joel bueno <jabs05.1991@gmail.com>
2018-11-27 14:37:57 -0400

    Begin

---
+++ Sistema/Sistema.Datos/Maping/Almacen/CategoriaMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Sistema.Entidades.Almacen;
+
+namespace Sistema.Datos.Maping.Almacen
+{
+    class CategoriaMap : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configuere (EntityTypeBuilder<Categoria> builder)
+        {
+
+        }
+
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.ToTable("categoria")
+                .HasKey(c => c.idcategoria);
+            builder.Property(c => c.idcategoria)
+                .HasMaxLength(50);
+            builder.Property(c => c.descripcion)
+                .HasMaxLength(256);
+        }
+    }
+}
